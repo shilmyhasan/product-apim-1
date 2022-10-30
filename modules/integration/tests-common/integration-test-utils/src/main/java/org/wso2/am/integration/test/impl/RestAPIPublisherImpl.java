7986080c2f4537f6419cb72eae31d4c2475833f9
Isuru Maduranga Wijesiri <isurumaduranga.16@cse.mrt.ac.lk>
2022-10-30 23:18:05 +0530

    Revert "Integration tests for PR carbon-apimgt#5427"

--- modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/impl/RestAPIPublisherImpl.java
+++ modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/impl/RestAPIPublisherImpl.java
@@ -49,7 +49,6 @@ import org.wso2.am.integration.clients.publisher.api.v1.GraphQlSchemaIndividualA
 import org.wso2.am.integration.clients.publisher.api.v1.OperationPoliciesApi;
 import org.wso2.am.integration.clients.publisher.api.v1.RolesApi;
 import org.wso2.am.integration.clients.publisher.api.v1.ScopesApi;
-import org.wso2.am.integration.clients.publisher.api.v1.SettingsApi;
 import org.wso2.am.integration.clients.publisher.api.v1.SubscriptionsApi;
 import org.wso2.am.integration.clients.publisher.api.v1.ThrottlingPoliciesApi;
 import org.wso2.am.integration.clients.publisher.api.v1.UnifiedSearchApi;
@@ -92,7 +91,6 @@ import org.wso2.am.integration.clients.publisher.api.v1.dto.ResourcePolicyListDT
 import org.wso2.am.integration.clients.publisher.api.v1.dto.ScopeDTO;
 import org.wso2.am.integration.clients.publisher.api.v1.dto.ScopeListDTO;
 import org.wso2.am.integration.clients.publisher.api.v1.dto.SearchResultListDTO;
-import org.wso2.am.integration.clients.publisher.api.v1.dto.SettingsDTO;
 import org.wso2.am.integration.clients.publisher.api.v1.dto.SubscriptionListDTO;
 import org.wso2.am.integration.clients.publisher.api.v1.dto.SubscriptionPolicyListDTO;
 import org.wso2.am.integration.clients.publisher.api.v1.dto.ThrottlingPolicyListDTO;
@@ -164,7 +162,6 @@ public class RestAPIPublisherImpl {
     private String disableVerification = System.getProperty("disableVerification");
     private ApiOperationPoliciesApi apisOperationPoliciesApi = new ApiOperationPoliciesApi();
     private OperationPoliciesApi operationPoliciesApi = new OperationPoliciesApi();
-    public SettingsApi settingsApi = new SettingsApi();
 
 
     @Deprecated
@@ -215,7 +212,6 @@ public class RestAPIPublisherImpl {
         apiAuditApi.setApiClient(apiPublisherClient);
         unifiedSearchApi.setApiClient(apiPublisherClient);
         sharedScopesApi.setApiClient(apiPublisherClient);
-        settingsApi.setApiClient(apiPublisherClient);
         operationPoliciesApi.setApiClient(apiPublisherClient);
         apisOperationPoliciesApi.setApiClient(apiPublisherClient);
         endpointCertificatesApi.setApiClient(apiPublisherClient);
@@ -1156,11 +1152,6 @@ public class RestAPIPublisherImpl {
                 null);
         Assert.assertEquals(HttpStatus.SC_CREATED, apiDtoApiResponse.getStatusCode());
         return apiDtoApiResponse.getData();
-    }
-
-    public ApiResponse<APIDTO> importOASDefinitionResponse(File file, String properties) throws ApiException {
-        ApiResponse<APIDTO> apiDtoApiResponse = apIsApi.importOpenAPIDefinitionWithHttpInfo(file, null, properties, null);
-        return apiDtoApiResponse;
     }
 
     public GraphQLValidationResponseDTO validateGraphqlSchemaDefinition(File schemaDefinition) throws ApiException {
@@ -2522,9 +2513,5 @@ public class RestAPIPublisherImpl {
             }
         }
 
-    }
-
-    public SettingsDTO getSettings() throws ApiException {
-        return settingsApi.getSettings();
     }
 }
