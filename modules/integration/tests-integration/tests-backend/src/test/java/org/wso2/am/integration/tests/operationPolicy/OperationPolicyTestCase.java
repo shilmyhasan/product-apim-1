e8aeb2220098482179fae489d1ee394b8c7c14f5
Crowley Rajapakse <srrajapakse1@gmail.com>
2024-01-05 14:02:35 +0530

    Revert "Add new AddHeader policy with regular expression fix"

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/operationPolicy/OperationPolicyTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/operationPolicy/OperationPolicyTestCase.java
@@ -196,12 +196,10 @@ public class OperationPolicyTestCase extends APIManagerLifecycleBaseTest {
         Map<String, Object> attributeMap = new HashMap<>();
         attributeMap.put("headerName", "TestHeader");
         attributeMap.put("headerValue", "TestValue");
-        List<OperationPolicyDTO> opList = getPolicyList(policyName, policyMap, attributeMap);
-        opList.get(0).setPolicyVersion("v2");
 
         APIOperationPoliciesDTO apiOperationPoliciesDTO = new APIOperationPoliciesDTO();
-        apiOperationPoliciesDTO.setRequest(opList);
-        apiOperationPoliciesDTO.setResponse(opList);
+        apiOperationPoliciesDTO.setRequest(getPolicyList(policyName, policyMap, attributeMap));
+        apiOperationPoliciesDTO.setResponse(getPolicyList(policyName, policyMap, attributeMap));
 
         apidto.getOperations().get(0).setOperationPolicies(apiOperationPoliciesDTO);
         restAPIPublisher.updateAPI(apidto);
@@ -327,11 +325,9 @@ public class OperationPolicyTestCase extends APIManagerLifecycleBaseTest {
             requestPolicyList.add(getPolicyList(policyList[i], policyMap, null).get(0));
             responsePolicyList.add(getPolicyList(policyList[i], policyMap, null).get(0));
         }
-        List<OperationPolicyDTO> opList = getPolicyList(policyName, policyMap, attributeMap);
-        opList.get(0).setPolicyVersion("v2");
 
-        requestPolicyList.add(opList.get(0));
-        responsePolicyList.add(opList.get(0));
+        requestPolicyList.add(getPolicyList(policyName, policyMap, attributeMap).get(0));
+        responsePolicyList.add(getPolicyList(policyName, policyMap, attributeMap).get(0));
         apiOperationPoliciesDTO.setRequest(requestPolicyList);
         apiOperationPoliciesDTO.setResponse(responsePolicyList);
         apidto.getOperations().get(0).setOperationPolicies(apiOperationPoliciesDTO);
